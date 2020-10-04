0e9e90bd45b09e98187a908d58abd76027cad9bd
longbeom <wowow1859@naver.com>
2020-10-04 21:25:23 +0900

    Update quiz-app UI

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -35,4 +35,26 @@ const quizData = [
         d: 'none of the above',
         correct: 'd'
     }
-]
+];
+
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+let currentQuestion = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuestion];
+    
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+    currentQuestion++;
+}
