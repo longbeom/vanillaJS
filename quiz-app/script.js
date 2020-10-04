608ed7e5ea35ec24341434f85dbc6ace62aa9a6f
longbeom <wowow1859@naver.com>
2020-10-04 14:03:06 +0900

    Add Quiz-App

---
+++ quiz-app/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is Hongbeom?',
+        a: '10',
+        b: '17',
+        c: '27',
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
+        question: 'Who is the President of Korea?',
+        a: 'Joo HongBeom',
+        b: 'Moon JaeIn',
+        c: 'Son HeungMin',
+        d: 'Kim JeongEun',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'd'
+    }
+]
